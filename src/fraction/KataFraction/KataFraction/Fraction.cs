064a6fe7d25b9830d23b004ddf1dca99e818ed0b
Christian Finel <macreiben@gmail.com>
2024-06-05 11:24:35 +0200

    fraction - red - changing return type.

--- src/fraction/KataFraction/KataFraction/Fraction.cs
+++ src/fraction/KataFraction/KataFraction/Fraction.cs
@@ -5,7 +5,7 @@ public class Fraction
     public int Numerator { get; } = 0;
     public int Denominator { get; } = 1;
 
-    public object Add(Fraction right)
+    public Fraction Add(Fraction right)
     {
         throw new NotImplementedException();
     }
