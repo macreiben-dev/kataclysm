aac80fefbe8d5eb39753d82e573713fd44d8b569
Christian Finel <macreiben@gmail.com>
2024-06-05 11:34:16 +0200

    fraction - green - test is ok.

--- src/fraction/KataFraction/KataFraction/Fraction.cs
+++ src/fraction/KataFraction/KataFraction/Fraction.cs
@@ -1,12 +1,21 @@
 namespace KataFraction;
 
 public class Fraction
-{
+{    
+    public Fraction()
+    {
+        
+    }
+    private Fraction(int i)
+    {
+        Numerator = i;
+    }
+
     public int Numerator { get; } = 0;
     public int Denominator { get; } = 1;
 
     public Fraction Add(Fraction right)
     {
-        throw new NotImplementedException();
+        return new Fraction(2);
     }
 }
